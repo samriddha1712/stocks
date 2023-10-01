079b418514be19f1aa0e57792ad61fa0e44e5504
Samriddha Biswas <samriddhabiswas1@gmail.com>
2023-10-01 14:45:37 +0530

    Deleted all waste files

--- main_groww.py
+++
@@ -1,6 +0,0 @@
-import requests as re
-
-response = re.get("https://groww.in/v1/api/stocks_data/v1/accord_points/exchange/NSE/segment/CASH/latest_prices_ohlc/20MICRONS.NS")
-data = response.json()
-live = data["ltp"]
-print(live)
--- testDate.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-today_date = datetime.date.today()
-today = today_date.strftime('%Y-%m-%d')
-
-print(today)
